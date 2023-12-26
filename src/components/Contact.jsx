a2acad082e2670abb63ea3e892bdbb866376a846
mariyamzee <zeenatmariyam85@gmail.com>
2023-12-26 23:46:03 +0530

    my first code commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import { Alert, Col, Container, Row } from "react-bootstrap";
+
+export function Contact(){
+    return(
+        <Container className="mt-5">
+        <Row>
+        <Col lg={12}>
+        <Alert variant="success mt-5">
+                <h1><center>OUR TEAM</center></h1>
+        </Alert>
+        </Col>
+        </Row>
+        </Container>
+    );
+}
